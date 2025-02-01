6920308c32b201eb634dad2649b645e421409a17
FailureDroid <marriottluke141@gmail.com>
2025-02-01 01:16:11 +0000

    camera locking and sensitivity multiplier
    I have added a lock on the y axis for when the player looks up and down and I have added a sensitivity multiplier that increase the look speed.

--- Assets/Scripts/Luke/CameraRotation.cs
+++ Assets/Scripts/Luke/CameraRotation.cs
@@ -8,17 +8,28 @@ public class CameraRotation : MonoBehaviour
     private float RotX = 0;
     private float RotY = 0;
 
+    // Sensitivity multiplier
+    public float sensitivity = 2.0f;
+
+    // Clamp values for vertical rotation
+    public float minY = -90f;
+    public float maxY = 90f;
+
     // Update is called once per frame
     void Update()
     {
         // Get mouse movement input
-       float MouseX = Input.GetAxis("Mouse X");
-       float MouseY = Input.GetAxis("Mouse Y");
-       transform.Rotate(-MouseY, MouseX, 0);
+        float MouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float MouseY = Input.GetAxis("Mouse Y") * sensitivity;
 
-       // Rotation angles And set game Object rotation
-       RotX = RotX + Input.GetAxis("Mouse X");
-       RotY = RotY + Input.GetAxis("Mouse Y");
-       transform.rotation = Quaternion.Euler(-RotY, RotX, 0);
+        // Update rotation angles
+        RotX += MouseX;
+        RotY -= MouseY;
+
+        // Clamp the vertical rotation
+        RotY = Mathf.Clamp(RotY, minY, maxY);
+
+        // Set the rotation of the camera
+        transform.rotation = Quaternion.Euler(RotY, RotX, 0);
     }
 }

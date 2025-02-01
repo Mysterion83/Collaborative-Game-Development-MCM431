18f1c6da835b46f4c4b3722af0877fae3dad7b23
FailureDroid <marriottluke141@gmail.com>
2025-02-01 01:46:41 +0000

    Serialize field and rigid body
    I have added a Serialize field and rigid body and the player would only move if the player object has a rigid body.

--- Assets/Scripts/Luke/PlayerMovement.cs
+++ Assets/Scripts/Luke/PlayerMovement.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
+    // Serialized variable for move speed
+    [SerializeField]
+    private float MoveSpeed = 5f;
+
+    // Reference to the Rigidbody component
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // Get the Rigidbody component attached to this GameObject
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -13,9 +24,15 @@ public class PlayerMovement : MonoBehaviour
         float MoveVertical = Input.GetAxisRaw("Vertical");
 
         // Create a movement vector
-        Vector3 movement = new Vector3(MoveHorizontal, 0f, MoveVertical).normalized;
+        Vector3 Movement = new Vector3(MoveHorizontal, 0f, MoveVertical).normalized;
 
-        // Move the player
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        // Move the player using the Rigidbody
+        MovePlayer(Movement);
+    }
+
+    private void MovePlayer(Vector3 Movement)
+    {
+        // Apply movement to the Rigidbody
+        rb.MovePosition(transform.position + Movement * MoveSpeed * Time.deltaTime);
     }
 }

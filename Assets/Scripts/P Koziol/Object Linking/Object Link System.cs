01b7a8544e274a6ba6a7494588a6299228c1ac21
Mysterion83 <0kozip63@solent.ac.uk>
2025-02-02 23:30:12 +0000

    Added Placeholder Materials
    - Imported Placeholder Materials from the unity asset store (https://assetstore.unity.com/packages/2d/textures-materials/gridbox-prototype-materials-129127)

--- Assets/Scripts/P Koziol/Object Linking/Object Link System.cs
+++ Assets/Scripts/P Koziol/Object Linking/Object Link System.cs
@@ -8,24 +8,33 @@ public class ObjectLinkSystem : MonoBehaviour
     ObjectLinkSystem LinkedObject;
     [SerializeField]
     bool IsPresentObject;
-    
-    
-    
+
+    [SerializeField]
+    float PreviousPositionDistanceLeeway = 0.0000001f;
+    [SerializeField]
+    Vector3 PreviousPosition;
+    [SerializeField]
+    Vector3 PreviousRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PreviousPosition = transform.localPosition;
     }
     private void FixedUpdate()
     {
+        
         if (LinkedObject == null)
         {
             Destroy(gameObject);
         }
-        if (!IsPresentObject && LinkedObject.transform.localPosition != transform.localPosition)
+        if (!IsPresentObject && LinkedObject.transform.localPosition != transform.localPosition && Vector3.Distance(transform.localPosition, PreviousPosition) > PreviousPositionDistanceLeeway)
         {
             Vector3 LocalPos = transform.localPosition;
             LinkedObject.transform.localPosition = LocalPos;
         }
+
+        PreviousPosition = transform.localPosition;
     }
 }

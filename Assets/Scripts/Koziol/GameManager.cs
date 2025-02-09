c7f723b33ea84a37eb9d78f930ce06b2c0a4f145
Mysterion83 <0kozip63@solent.ac.uk>
2025-02-09 19:02:05 +0000

    Game Manager
    - Implemented a Game Manager stub

---
+++ Assets/Scripts/Koziol/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get { return _instance; } }
+    private static GameManager _instance;
+
+
+    private void Start()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            _instance = this;
+        }
+    }
+}

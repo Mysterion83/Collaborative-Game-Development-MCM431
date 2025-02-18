ead5a3c287b2fcbb857db74697eb5ae7e52c7900
Mysterion83 <0kozip63@solent.ac.uk>
2025-02-18 00:39:21 +0000

    New Interactables
    - Implemented a new single use button interactable, sets the tag to none to removed interactions
    - Implemented a switch interactable which stores a bool on or off state and allows for retrieval

---
+++ Assets/Scripts/Koziol/Interaction/Interactable Switch.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSwitch : Interactable
+{
+    public bool State { get { return _state; } }
+    private bool _state;
+    public override void Interact()
+    {
+        _state = !_state;
+    }
+    public override void ScrollInteract(float mouseScrollDelta)
+    {
+        return;
+    }
+}

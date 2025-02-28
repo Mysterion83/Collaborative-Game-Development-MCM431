a79c2117ca7d7e349635ac4e45ded7c6b5822be5
MightyMango42 <0benns60@solent.ac.uk>
2025-02-28 12:53:46 +0000

    Prefab Creation and Script Assigning
    - Created Planet Prefab
    - Assigned Planet Puzzle Manager script to a planet container
    - Assigned each planet to the Planet[] Array within the manager script

--- Assets/Scripts/PlanetPuzzle/PlanetPuzzleManager.cs
+++ Assets/Scripts/PlanetPuzzle/PlanetPuzzleManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlanetPuzzleManager : MonoBehaviour
 {
+    [SerializeField] private Planet[] planets; 
+    public bool isSolved;
+
     // Start is called before the first frame update
     void Start()
     {

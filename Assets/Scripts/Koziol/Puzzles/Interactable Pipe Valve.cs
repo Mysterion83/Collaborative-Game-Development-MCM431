54e9188c38bb52e0d4bfefea2c2e1492d3500dbf
Mysterion83 <0kozip63@solent.ac.uk>
2025-03-11 23:58:09 +0000

    Script Renaming
    - Renamed Pipe Valve to Interactable Pipe Valve

--- Assets/Scripts/Koziol/Puzzles/Interactable Pipe Valve.cs
+++ Assets/Scripts/Koziol/Puzzles/Interactable Pipe Valve.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeValve : InteractableSwitch
+public class InteractablePipeValve : InteractableSwitch
 {
     public override void Interact()
     {

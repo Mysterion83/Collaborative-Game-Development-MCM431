84877de04f57382fe3a826b66efe051671ab0e8c
Tired-DJ <jrahmedwork@gmail.com>
2025-03-28 14:05:47 +0000

    Updated Credits
    Updated Credits list
    Added ability to terminate credits early in the credits scene

---
+++ Assets/Scripts/Jess (DJ)/CreditsController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour
+{
+    Animator CreditsAnimator;
+
+    void Start()
+    {
+        CreditsAnimator = GetComponent<Animator>();
+    }
+
+    // Loads main menu once credits animation is over
+    void Update()
+    {
+        if (CreditsAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            Debug.Log("Credits animation ended");
+            SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("User terminated credits");
+            SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+        }
+    }
+
+}

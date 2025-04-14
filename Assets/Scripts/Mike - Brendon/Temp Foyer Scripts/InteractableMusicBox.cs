5df14ba39049f38bcab72066ba63c46e09a2d363
Frae21 <fraezz@outlook.com>
2025-04-14 15:20:56 +0100

    Added Music Box UI
    - Test problems

--- Assets/Scripts/Mike - Brendon/Temp Foyer Scripts/InteractableMusicBox.cs
+++ Assets/Scripts/Mike - Brendon/Temp Foyer Scripts/InteractableMusicBox.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractableMusicBox : Interactable
 {
     [SerializeField]
     private LevelTeleportSystem _levelTeleportSystem;
+    public GameObject musicBoxUI; 
 
     public override void Interact()
     {
         _levelTeleportSystem.IsAllowedToTeleport = true;
+        musicBoxUI.SetActive(true);
         Destroy(gameObject);
     }
 

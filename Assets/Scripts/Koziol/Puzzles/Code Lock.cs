749ee8f75a75a117586ecba3505c4bf23f90a6e3
MightyMango42 <0benns60@solent.ac.uk>
2025-04-25 13:46:14 +0100

    Updated Combination Lock
    - Updated combination lock prefab to display a string of all the dial values

--- Assets/Scripts/Koziol/Puzzles/Code Lock.cs
+++ Assets/Scripts/Koziol/Puzzles/Code Lock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CodeLock : Interactable
@@ -10,6 +11,8 @@ public class CodeLock : Interactable
     [SerializeField]
     private InteractableDial[] Dials;
 
+    [SerializeField] TextMeshPro displayValues;
+
     private void Start()
     {
         Dials = GetComponentsInChildren<InteractableDial>();
@@ -17,6 +20,8 @@ public class CodeLock : Interactable
 
     public override void Interact()
     {
+        UpdateCodeText();
+
         if (CheckCode())
         {
             Debug.Log("Code is correct");
@@ -40,14 +45,28 @@ public class CodeLock : Interactable
         {
             if (Dials[i].GetDialState() != code[i])
             {
+                UpdateCodeText();
                 return false;
             }
         }
+        displayValues.text = string.Empty;
         return true;
     }
 
     public override void ScrollInteract(float mouseScrollDelta)
     {
         return;
+    }
+
+    private void UpdateCodeText()
+    {
+        string newCodeText = "";
+
+        for (int i = 0; i < Dials.Length; i++)
+        {
+            int currentDialValue = Dials[i].GetDialState();
+            newCodeText += currentDialValue.ToString();
+            displayValues.text = newCodeText;
+        }
     }
 }
